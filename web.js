7e80a0423890e12333099823152262c010ebbe62
Munair Simpson <munair@gmail.com>
2015-05-09 01:18:18 +0000

    [AUTOMATED] Pushing changes to KR website from COM website excluding index.html in particular. To modify index.html git clone the repo and modify by hand.

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
+var http = require('http');
 var fs = require('fs');
-var postmark = require("postmark")("db375280-7dd3-4240-89db-1e19ee9e939e")
 
 var app = express();
 
@@ -14,44 +14,71 @@ app.use("/sounds", express.static(__dirname + '/sounds'));
 
 app.get('/', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/capoeira', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/music', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/membership', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/faq', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/contact', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/email', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/formconfirmation', function(request, response) { var htmlBuffer = fs.readFileSync('index.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/inc_capoeira.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_capoeira.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/inc_music.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_music.html', 'utf-8'); response.send(htmlBuffer); });
+app.get('/inc_membership.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_membership.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/inc_faq.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_faq.html', 'utf-8'); response.send(htmlBuffer); });
-app.get('/inc_contact.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_contact.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/inc_email.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_email.html', 'utf-8'); response.send(htmlBuffer); });
 app.get('/inc_formconfirmation.html', function(request, response) { var htmlBuffer = fs.readFileSync('inc_formconfirmation.html', 'utf-8'); response.send(htmlBuffer); });
 
 app.post('/inc_email.html', function(request, response) {
   var name = request.body.name;
+  var fbid = request.body.fbid;
   var email = request.body.email;
   var mobile = request.body.mobile;
-  var message = request.body.message;
+  var medical = request.body.medical;
+  var referral = request.body.referral;
+  var background = request.body.background;
   var validation = request.body.validation;
-  var out = 'contact name: ' + name 
-          + '\ncontact email: ' + email 
+  var out = 'name: ' + name 
+          + '\nfbid: ' + fbid
+          + '\nemail: ' + email 
           + '\nmobile: ' + mobile 
-          + '\nmessage: ' + message
+          + '\nbackground: ' + background
+          + '\nmedical: ' + medical
+          + '\nreferral: ' + referral
           + '\nvalidation: ' + validation 
           + '\n';
+  var options = {
+    host: 'api.postmarkapp.com',
+    port: 80,
+    path: '/email',
+    method: 'POST',
+    headers: { 
+      'accept': 'application/json',
+      'content-type': 'application/json',
+      'x-postmark-server-token': 'f2392957-2e91-4ace-9f07-a72da382dd4c'
+    }
+  };
 
-  postmark.send({
-    "From" : "munair@quilombolarecreation.kr",
-    "To" : "munair@quilombolarecreation.kr",
-    "Subject" : "Contact from www.quilombolarecreation.kr",
-    "Tag" : "Inquiry",
-    "TextBody" : out
-  }, function(error, success) {
-      if(error) {
-          console.error("Unable to send via postmark: " + error.message);
-         return;
-      }
-      console.info("Sent to postmark for delivery")
-  });
+  if (validation === "i am human") {
+    var req = http.request(options, function(res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+      res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+    });
 
-  response.redirect('/inc_formconfirmation.html');
+    req.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+
+    // write data to request body
+    req.write("{From: 'munair@quilombola.com', To: 'munair@gmail.com', Subject: 'Membership Application from www.quilombolarecreation.kr', TextBody: '");
+    req.write(out);
+    req.write("'}");
+    req.end();
+
+    response.redirect('/inc_formconfirmation.html');
+  } else {
+    response.redirect('/inc_email.html');
+  }
+
 });
 
 var port = process.env.PORT || 8080;
